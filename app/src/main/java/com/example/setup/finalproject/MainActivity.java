46cefc7c5b834a3af4612f104a7244dfce2542bc
Ina Maloney <Ina@Inas-MacBook-Air.local>
2016-10-27 16:49:59 -0400

    initial commit

---
+++ app/src/main/java/com/example/setup/finalproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.setup.finalproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button add = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        add = (Button) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Add.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
