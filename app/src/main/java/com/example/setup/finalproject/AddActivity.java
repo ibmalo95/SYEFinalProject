3b66c682b82add06407f3d1c01cbc104816e98ec
Ina Maloney <Ina@Inas-MacBook-Air.local>
2016-10-30 13:44:46 -0400

    Get college name from add activity

--- app/src/main/java/com/example/setup/finalproject/AddActivity.java
+++ app/src/main/java/com/example/setup/finalproject/AddActivity.java
@@ -1,21 +1,41 @@
 package com.example.setup.finalproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class AddActivity extends Activity {
 
+    public static final String ADD = "com.example.setup.AddActivity.ADD";
     EditText college_name = null;
+    Button add_college = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        // Get the college that the user put in and send it back to MainActivity
-        college_name = (EditText) findViewById(R.id.college_name);
-        String result = college_name.getText().toString();
+        add_college = (Button) findViewById(R.id.add_college);
+        add_college.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the college that the user put in and send it back to MainActivity
+                college_name = (EditText) findViewById(R.id.college_name);
+                String college = college_name.getText().toString();
+                if (!college.isEmpty()) {
+                    Intent result = new Intent();
+                    result.putExtra(ADD, college);
+                    setResult(RESULT_OK, result);
+                    finish();
+                }
+            }
+        });
+
+
+
 
     }
 }
--- app/src/main/java/com/example/setup/finalproject/MainActivity.java
+++ app/src/main/java/com/example/setup/finalproject/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.setup.finalproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     public static final int ADD_REQUEST = 1;
+    public static final String LOG_TAG = MainActivity.class.getName();
 
     Button add = null;
+    String college = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,19 @@ public class MainActivity extends Activity {
                 startActivityForResult(intent, ADD_REQUEST);
             }
         });
+    }
+
+    // Result from NoteActivity gets processed here
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == ADD_REQUEST && resultCode == RESULT_OK) {
+            college = result.getStringExtra(AddActivity.ADD);
+            Log.i(LOG_TAG, college);
+        }
+    }
+
+    // TODO: add college to the ListView
+    // Need a reference to the fragment
+    protected void addCollege() {
 
     }
 }
