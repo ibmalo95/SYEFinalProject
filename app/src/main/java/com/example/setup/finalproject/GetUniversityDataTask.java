4cdb13860b8e13df33b0b9eb58385834abb8ffae
Ina Maloney <ibmalo13@stlawu.edu>
2016-11-15 11:16:40 -0500

    get data from UDED

--- app/src/main/java/com/example/setup/finalproject/GetUniversityDataTask.java
+++ app/src/main/java/com/example/setup/finalproject/GetUniversityDataTask.java
@@ -1,6 +1,20 @@
 package com.example.setup.finalproject;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by Ina on 11/15/16.
@@ -8,10 +22,65 @@ import android.os.AsyncTask;
 
 public class GetUniversityDataTask extends AsyncTask<String, Void, String []>{
 
-    
+    private static final String LOG_TAG = GetUniversityDataTask.class.getName();
 
     @Override
-    protected String[] doInBackground(String... params) {
+    protected String[] doInBackground(String... url) {
+
+        HttpsURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+
+        try {
+            // get the URL and make a connection
+            URL uri = new URL(url[0]);
+            urlConnection = (HttpsURLConnection) uri.openConnection();
+            urlConnection.connect();
+
+            // read the input stream
+            InputStream inputStream = urlConnection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Build the JSON
+            String line = null;
+            StringBuilder build = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                build.append(line);
+            }
+            // JSON of institution with INSTNM, WEBADDR, LATITUDE, & LONGITUD
+            String UniversityJSON = build.toString();
+            Log.i(LOG_TAG, UniversityJSON);
+            ArrayList<String> UniversityList = getRecordArrayFromJSON(UniversityJSON);
+
+            //Log.i(LOG_TAG, UniversityList.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return new String[0];
     }
+
+    protected ArrayList<String> getRecordArrayFromJSON(String JSON) throws JSONException {
+        JSONObject universityInfo = null;
+        // Full JSON
+        universityInfo = new JSONObject(JSON);
+        // Results object
+        JSONObject results = universityInfo.getJSONObject("result");
+        // Records array: has name, university url, and location data
+        JSONArray records = results.getJSONArray("records");
+
+        ArrayList<String> universityList = new ArrayList();
+        JSONObject info = records.getJSONObject(0);
+        universityList.add(info.getString("INSTNM"));
+        universityList.add(info.getString("WEBADDR"));
+        universityList.add(info.getString("LATITUDE"));
+        universityList.add(info.getString("LONGITUD"));
+
+        return universityList;
+    }
+
+
 }
